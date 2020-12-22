a0420775ec4b1b7da6fb52a033e97238a9532f8b
Elad Avron <eladavron@gmail.com>
2020-12-22 22:42:27 +0000

    Fixed bedtime

--- CosmoGolem/bedtime.py
+++ CosmoGolem/bedtime.py
@@ -134,9 +134,9 @@ class Bedtime(commands.Cog):
             bool: True if tomorrow morning is a weekend (Saturday or Sunday)
         """
         if relative_to_time.hour < morning:  # Morning is today
-            return relative_to_time.weekday >= 5  # 5 is Saturday, 6 is Sunday
+            return relative_to_time.weekday() >= 5  # 5 is Saturday, 6 is Sunday
         # Morning is tomorrow
-        return relative_to_time.weekday > 4  # If today is Friday (4), tomorrow morning is a weekend.
+        return relative_to_time.weekday() > 4  # If today is Friday (4), tomorrow morning is a weekend.
 
 
 def setup(bot):
